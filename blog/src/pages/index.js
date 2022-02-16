431610a3e5ae16120644afe0b0c59f07486c9f21
SS <soldas1@gmatc.matc.edu>
2022-02-16 14:03:56 -0600

    Completed Activity 3

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { graphql } from 'gatsby';
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -13,6 +14,15 @@ const IndexPage = ({ data }) => (
       data.allContentfulBlogPost.edges.map(edge => (
         <li>
           <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
+          <div>
+            <GatsbyImage 
+              image={edge.node.heroImage.gatsbyImageData}
+              alt="test"
+              />
+          </div>
+          <div>
+            {edge.node.body.childMarkdownRemark.excerpt}
+          </div>
         </li>
       ))
     }
@@ -23,15 +33,27 @@ const IndexPage = ({ data }) => (
 export default IndexPage
 
 export const query = graphql`
-  {
-    allContentfulBlogPost {
-      edges {
-        node {
-          id
-          title
-          slug
+{
+  allContentfulBlogPost {
+    edges {
+      node {
+        id
+        title
+        slug
+        body {
+          childMarkdownRemark {
+            excerpt
+          }
         }
-      } 
-    }
+        heroImage {
+          gatsbyImageData(
+            layout:CONSTRAINED
+            placeholder:BLURRED
+            width: 300
+          )
+        }
+      }
+    } 
   }
+}
 `
