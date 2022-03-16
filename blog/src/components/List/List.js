52be96e9cd88789e3ab55899d3a971cfdec3d97f
SS <soldas1@gmatc.matc.edu>
2022-03-16 18:10:47 -0500

    Completed Activity 6

---
+++ blog/src/components/List/List.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { BaseContainer } from '../BaseContainer'
+
+export const List = ({ children, ...rest }) => (
+    <BaseContainer
+    flex
+    flexDirection='column'
+    mx='auto'
+    as='ul'
+    {...rest}
+    >
+    {children}
+    </BaseContainer>
+)
